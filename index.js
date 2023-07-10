cb3fd6373724ed4efb042f6ee68abcd96c41a202
OleksandrNovyk <novykav@gmail.com>
2023-07-10 09:45:00 +0300

    1

--- index.js
+++ index.js
@@ -19,32 +19,36 @@ program.parse(process.argv);
 const argv = program.opts();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
-  switch (action) {
-    case "list":
-      const contacts = await listContacts();
-      console.table(contacts);
-      break;
-
-    case "get":
-      const contactById = await getContactById(id);
-      if (!contactById) {
-        throw new Error(`Contact with this id not found`);
-      }
-      console.table(contactById);
-      break;
-
-    case "add":
-      const newContact = await addContact(name, email, phone);
-      console.table(newContact);
-      break;
-
-    case "remove":
-      const deletedContact = await removeContact(id);
-      console.table(deletedContact);
-      break;
-
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+  try {
+    switch (action) {
+      case "list":
+        const contacts = await listContacts();
+        console.table(contacts);
+        break;
+  
+      case "get":
+        const contactById = await getContactById(id);
+        if (!contactById) {
+          throw new Error(`Contact with this id not found`);
+        }
+        console.table(contactById);
+        break;
+  
+      case "add":
+        const newContact = await addContact(name, email, phone);
+        console.table(newContact);
+        break;
+  
+      case "remove":
+        const deletedContact = await removeContact(id);
+        console.table(deletedContact);
+        break;
+  
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
 
